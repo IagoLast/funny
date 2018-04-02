4b11621680d1ec877bbb42d249eae41258afb865
IagoLast <iago.lastra@gmail.com>
2018-04-02 22:22:56 +0200

    Remove banana animation on click

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
   'use strict';
 
   angular
@@ -9,7 +9,7 @@
   function MainController($window, $location, $scope, $rootScope, $timeout) {
 
 
-    $timeout(function() {
+    $timeout(function () {
       $rootScope.mainClass = 'slide-up';
     }, 1000);
     $window.addEventListener('wheel', _onWheel);
@@ -19,15 +19,30 @@
     var b2 = angular.element($window.document.querySelector('.b2 img'));
     var b3 = angular.element($window.document.querySelector('.b3 img'));
 
-    animateBanana(b1);
-    animateBanana(b2);
-    animateBanana(b3);
+    $timeout(function () {
+      var parent = angular.element(b2.parent());
+      parent.addClass('animate');
+      $timeout(function () {
+        parent.removeClass('animate');
+      }, 2000);
+    }, 2000);
 
+    $timeout(function () {
+      var parent = angular.element(b3.parent());
+      parent.addClass('animate');
+      $timeout(function () {
+        parent.removeClass('animate');
+      }, 2000);
+    }, 6000);
 
+    $timeout(function () {
+      var parent = angular.element(b1.parent());
+      parent.addClass('animate');
+    }, 9000);
 
 
     function _onWheel(e) {
-      $scope.$evalAsync(function() {
+      $scope.$evalAsync(function () {
         if (e.deltaY > 0) {
           $window.removeEventListener('wheel', _onWheel);
           $location.path('/projects');
@@ -36,15 +51,6 @@
     }
 
 
-    function animateBanana(banana) {
-      banana.on('click', function() {
-        var parent = angular.element(banana.parent());
-        parent.addClass('animate');
-        $timeout(function() {
-          parent.removeClass('animate');
-        }, 1000);
-      });
-    }
   }
 
 
